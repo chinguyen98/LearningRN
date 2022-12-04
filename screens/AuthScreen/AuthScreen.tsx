74f00f76f5cda5ac327671a0ad98bdd981accc76
Chi Nguyen Dac <dacchi6761@gmail.com>
2022-12-04 23:08:52 +0700

    update

--- screens/AuthScreen/AuthScreen.tsx
+++ screens/AuthScreen/AuthScreen.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, ViewStyle } from 'react-native';
 import BaseInput from '../../components/BaseInput';
 import useThemeStore from '../../stores/theme.store';
 
 const AuthScreen = () => {
   const theme = useThemeStore((state) => state.base.theme);
 
+  const style: ViewStyle = {
+    ...theme,
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
   return (
-    <SafeAreaView style={theme}>
+    <SafeAreaView style={style}>
       <View>
         <Text className="text-center text-lg">LOGIN</Text>
-        <BaseInput placeholder="Input your username" />
-        <BaseInput placeholder="Input your password" type="password" />
+        <View className="my-3 px-3">
+          <BaseInput placeholder="Input your username" />
+          <BaseInput placeholder="Input your password" type="password" />
+        </View>
       </View>
     </SafeAreaView>
   );
